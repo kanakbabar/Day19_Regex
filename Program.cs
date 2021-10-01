3121ce4858ee0e355f605b5c3bce8cd126193278
kanakbabar <kanakbabar999@gamil.com>
2021-10-01 22:21:51 +0530

    [kanak]ADD : user ragistration problem

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day19_RegexProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uc1_uc3 uc1_uc3 = new uc1_uc3();
+
+           // Console.WriteLine(uc1_uc3.validatePinCode("abc.xyz@bl.co.in"));  //uc1  To uc3
+
+            //  Console.WriteLine(uc1_uc3.validatePinCode("91 1234567899"));    //uc4
+
+            Console.WriteLine(uc1_uc3.validatePinCode("Kanak@12"));    //uc5 To  uc9
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ uc1_uc3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_RegexProblem
+{
+    class uc1_uc3
+    {
+        // public static string Regex_PinCode = "^[a-zA-z]{3}[.][a-zA-Z]{3}[@][a-zA-z]{2}([.][a-zA-Z]{2})+$";   //uc1 To uc3
+
+       // public static string Regex_PinCode = "^[1-9]{2}[ ][0-9]{10}$";    //uc4
+
+        public static string Regex_PinCode = "^[a-zA-Z]{5}[@][1-9]{2}$";
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, Regex_PinCode);
+        }
+       
+    }
+}
