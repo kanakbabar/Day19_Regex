8cfdd67738cad5a40c42742cd7d54e987e39826e
kanakbabar <kanakbabar999@gamil.com>
2021-10-01 22:17:42 +0530

    [kanak]ADD : Email problem

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regexpattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            patterns patterns = new patterns();
+            Console.WriteLine((patterns.validatePinCode("abc@yahoo.com")));
+            Console.WriteLine((patterns.validatePinCode("abc-100@yahoo.com")));
+            Console.WriteLine((patterns.validatePinCode("abc111@yahoo.com")));
+            Console.WriteLine((patterns.validatePinCode("abc.100@yahoo.com.net")));
+            Console.WriteLine((patterns.validatePinCode("abc.100@yahoo.com.net.au")));
+            Console.WriteLine((patterns.validatePinCode("abc@1.com")));
+            Console.WriteLine((patterns.validatePinCode("abc@gmail.com.com")));
+            Console.WriteLine((patterns.validatePinCode("abc+100@gmail.com")));
+            //false Email id
+            Console.WriteLine((patterns.validatePinCode("abc")));
+            Console.WriteLine((patterns.validatePinCode("abc@.com.my")));
+            Console.WriteLine((patterns.validatePinCode("abc123@gmail.a")));
+            Console.WriteLine((patterns.validatePinCode("abc123@.com")));
+            Console.WriteLine((patterns.validatePinCode("abc123@.com.com")));
+            Console.WriteLine((patterns.validatePinCode("abc@abc.com")));
+            Console.WriteLine((patterns.validatePinCode("abc()*@gmail.com")));
+            Console.WriteLine((patterns.validatePinCode("abc@%*.com")));
+            Console.WriteLine((patterns.validatePinCode("abc..2002@gmail.com")));
+            Console.WriteLine((patterns.validatePinCode("abc.@gmail.com")));
+            Console.WriteLine((patterns.validatePinCode("abc@abc@gmail.com")));
+            Console.WriteLine((patterns.validatePinCode("abc@gmail.com.1a")));
+            Console.WriteLine((patterns.validatePinCode("abc@gmail.com.aa.au")));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regexpattern
+{
+    class patterns
+    {
+        public static string Regex_PinCode = "^[a-zA-Z0-9]+([.-+][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z0-9]{2,3}([.][a-zA-Z]{2})?$";
+
+        
+
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, Regex_PinCode);
+        }
+    }
+}
